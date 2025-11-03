ea19bd94cdb3d6433aad74cd2b67ff65223ca61b
Nathalia Bezerra <nathalyabezerra@live.com>
2025-11-02 21:59:16 -0300

    Adiciona nome do médico à listagem de atendimento e adiciona botão que trocará status

--- app/components/tableAgendamentos.tsx
+++ app/components/tableAgendamentos.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Table } from "@radix-ui/themes";
-import { Agendamento } from "../lib/types";
+import { Badge, IconButton, Table } from "@radix-ui/themes";
+import { Agendamento, Medico } from "../lib/types";
+import { CheckCircledIcon } from "@radix-ui/react-icons";
 
 interface ProximasConsultasProps {
   limit?: number;
@@ -10,21 +11,29 @@ interface ProximasConsultasProps {
 export default function ProximasConsultas({ limit }: ProximasConsultasProps) {
   const [agendamentos, setAgendamentos] = useState<Agendamento[]>([]);
   const [loading, setLoading] = useState(true);
+  const [medicos, setMedicos] = useState<Medico[]>([]);
 
   useEffect(() => {
-    async function fetchAgendamentos() {
+    async function fetchData() {
       try {
-        const res = await fetch("/api/agendamentos");
-        const data: Agendamento[] = await res.json();
-        setAgendamentos(data);
+        const [agRes, medRes] = await Promise.all([
+          fetch("/api/agendamentos"),
+          fetch("/api/medicos"),
+        ]);
+
+        const agData: Agendamento[] = await agRes.json();
+        const medData: Medico[] = await medRes.json();
+
+        setAgendamentos(agData);
+        setMedicos(medData);
       } catch (error) {
-        console.error("Erro ao buscar agendamentos:", error);
+        console.error("Erro ao buscar dados:", error);
       } finally {
         setLoading(false);
       }
     }
 
-    fetchAgendamentos();
+    fetchData();
   }, []);
 
   if (loading) return <p>Carregando...</p>;
@@ -33,15 +42,34 @@ export default function ProximasConsultas({ limit }: ProximasConsultasProps) {
     ? agendamentos.slice(0, limit)
     : agendamentos;
 
+  const getMedicoNome = (id: number) => {
+    const medico = medicos.find((m) => m.id === id);
+    return medico ? medico.nome : "—";
+  };
+
   return (
-    <div className="w-full max-h-[85vh] rounded flex flex-col overflow-auto">
+    <div className="w-full max-h-[85vh] rounded flex flex-col overflow-auto px-10 mr-5">
       <Table.Root>
         <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>Data</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Hora</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Paciente</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Médico</Table.ColumnHeaderCell>
+          <Table.Row align={"center"}>
+            <Table.ColumnHeaderCell justify={"center"}>
+              Data
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell justify={"center"}>
+              Hora
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell justify={"center"}>
+              Paciente
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell justify={"center"}>
+              Médico
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell justify={"center"}>
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell justify={"center"}>
+              Ação
+            </Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
 
@@ -60,10 +88,26 @@ export default function ProximasConsultas({ limit }: ProximasConsultasProps) {
 
             return (
               <Table.Row key={index}>
-                <Table.RowHeaderCell>{dataFormatada}</Table.RowHeaderCell>
-                <Table.Cell>{horaFormatada}</Table.Cell>
-                <Table.Cell>{agendamento.pacienteNome}</Table.Cell>
-                <Table.Cell>{agendamento.medicoId}</Table.Cell>
+                <Table.Cell align={"center"}>{dataFormatada}</Table.Cell>
+                <Table.Cell align={"center"}>{horaFormatada}</Table.Cell>
+                <Table.Cell align={"center"}>
+                  {agendamento.pacienteNome}
+                </Table.Cell>
+                <Table.Cell align={"center"}>
+                  {getMedicoNome(agendamento.medicoId)}
+                </Table.Cell>
+                <Table.Cell align={"center"}>
+                  {agendamento.status === "agendado" ? (
+                    <Badge color="blue">Agendado</Badge>
+                  ) : (
+                    <Badge color="green">Atendido</Badge>
+                  )}
+                </Table.Cell>
+                <Table.Cell align={"center"}>
+                  <IconButton color="crimson" variant="soft">
+                    <CheckCircledIcon width="18" height="18" />
+                  </IconButton>
+                </Table.Cell>
               </Table.Row>
             );
           })}
