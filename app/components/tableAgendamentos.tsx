a2a55f0549c10a73ad89a2bce16fa177a4d93786
Nathalia Bezerra <nathalyabezerra@live.com>
2025-11-02 21:24:23 -0300

    Muda exibição de data e hora na tabela de agendamentos

--- app/components/tableAgendamentos.tsx
+++ app/components/tableAgendamentos.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import Link from "next/link";
-import { Flex, Table } from "@radix-ui/themes";
+import { Table } from "@radix-ui/themes";
 import { Agendamento } from "../lib/types";
 
 interface ProximasConsultasProps {
@@ -39,6 +38,7 @@ export default function ProximasConsultas({ limit }: ProximasConsultasProps) {
       <Table.Root>
         <Table.Header>
           <Table.Row>
+            <Table.ColumnHeaderCell>Data</Table.ColumnHeaderCell>
             <Table.ColumnHeaderCell>Hora</Table.ColumnHeaderCell>
             <Table.ColumnHeaderCell>Paciente</Table.ColumnHeaderCell>
             <Table.ColumnHeaderCell>Médico</Table.ColumnHeaderCell>
@@ -46,13 +46,27 @@ export default function ProximasConsultas({ limit }: ProximasConsultasProps) {
         </Table.Header>
 
         <Table.Body>
-          {agendamentosExibir.map((agendamento, index) => (
-            <Table.Row key={index}>
-              <Table.RowHeaderCell>{agendamento.dataHora}</Table.RowHeaderCell>
-              <Table.Cell>{agendamento.pacienteNome}</Table.Cell>
-              <Table.Cell>{agendamento.medicoId}</Table.Cell>
-            </Table.Row>
-          ))}
+          {agendamentosExibir.map((agendamento, index) => {
+            const data = new Date(agendamento.dataHora);
+            const dataFormatada = data.toLocaleDateString("pt-BR", {
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+            });
+            const horaFormatada = data.toLocaleTimeString("pt-BR", {
+              hour: "2-digit",
+              minute: "2-digit",
+            });
+
+            return (
+              <Table.Row key={index}>
+                <Table.RowHeaderCell>{dataFormatada}</Table.RowHeaderCell>
+                <Table.Cell>{horaFormatada}</Table.Cell>
+                <Table.Cell>{agendamento.pacienteNome}</Table.Cell>
+                <Table.Cell>{agendamento.medicoId}</Table.Cell>
+              </Table.Row>
+            );
+          })}
         </Table.Body>
       </Table.Root>
     </div>
